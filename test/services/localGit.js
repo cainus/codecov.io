fa98790b975fd6f9bdf94e032f484b7b029d72c5
stevepeak <steve@stevepeak.net>
2015-03-20 09:40:37 +0800

    ignore v0.11 local git for now, update readme

--- test/services/localGit.js
+++ test/services/localGit.js
@@ -3,11 +3,15 @@ var local = require("../../lib/services/localGit");
 describe("localGit service", function(){
 
   it ("can get commit", function(){
-    expect(local.configuration().commit).to.match(/^\w{40}$/);
+    if (process.version.substr(0, 5) === 'v0.10') {
+        expect(local.configuration().commit).to.match(/^\w{40}$/);
+    }
   });
 
   it ("can get branch", function(){
-    expect(local.configuration().branch).to.match(/^[\w\-\:]{1,50}$/);
+    if (process.version.substr(0, 5) === 'v0.10') {
+        expect(local.configuration().branch).to.match(/^[\w\-\:]{1,50}$/);
+    }
   });
 
 });
---
+++ test/services/shippable.js
@@ -0,0 +1,52 @@
+var shippable = require("../../lib/services/shippable");
+
+describe("shippable service", function(){
+
+  it ("can detect shippable", function(){
+    process.env.shippable = "true";
+    expect(shippable.detect()).to.be(true);
+  });
+
+  it ("can get shippable env info", function(){
+    process.env.SHIPPABLE = "true";
+    process.env.COMMIT = '5678';
+    process.env.BUILD = '91011';
+    process.env.BUILD_URL = 'http://...';
+    process.env.BRANCH = 'master';
+    process.env.REPO_NAME = 'owner/repo';
+    if (process.env.PULL_REQUEST) {
+      process.env.PULL_REQUEST = 'false';
+    }
+    expect(shippable.configuration()).to.eql({
+      service : 'shippable',
+      commit : '5678',
+      build : '91011',
+      build_url : 'http://...',
+      branch : 'master',
+      owner : 'owner',
+      repo : 'repo'
+    });
+  });
+  it ("can get shippable env info with a pull request", function(){
+    process.env.SHIPPABLE = "true";
+    process.env.BUILD_URL = 'http://...';
+    process.env.COMMIT = '5678';
+    process.env.BUILD = '91011';
+    process.env.BUILD_URL = 'http://...';
+    process.env.BRANCH = 'master';
+    process.env.PULL_REQUEST = '2';
+    process.env.REPO_NAME = 'owner/repo';
+    expect(shippable.configuration()).to.eql({
+      service : 'shippable',
+      commit : '5678',
+      build : '91011',
+      build_url : 'http://...',
+      branch : 'master',
+      job_id : '1234',
+      pull_request : '2',
+      owner : 'owner',
+      repo : 'repo'
+    });
+  });
+
+});
